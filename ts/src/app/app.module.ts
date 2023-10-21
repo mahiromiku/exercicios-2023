e1b40dbfcd088d0196a582d895d249e51d753879
Lucas <lucas25th@gmail.com>
2023-10-21 12:04:00 -0300

    teste pratico sem refatoracao

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser';
 
+import {TrabalhoComponent} from '../app/pages/trabalho/trabalho.component'
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: '', component: TrabalhoComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrabalhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
